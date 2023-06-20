a1059c8811320bfa3b27e1b57d82431a4f52f079
unknown <truptic.xworkz@gmail.com>
2023-06-20 13:50:35 +0530

    test-push

---
+++ ArrayClass.java
@@ -0,0 +1,19 @@
+public class ArrayClass{
+public static void main(String[] args)
+{
+int[] noofstudents ={10,20,30,40}; //literals
+float[] cgpaofstudents = new float[15];
+cgpaofstudents[0] = newfloat[5];
+for(int index =0 ; index>cgpaofstudents.length ; index--){
+System.out.println(cgpaofstudents[index]);
+}
+
+System.out.println(noofstudents[0]);
+char[] vowels ={'A', 'E', 'I', 'O', 'U'};
+System.out.println(vowels);
+for(int i=0; i<5; i++){
+System.out.println(vowels[i]);
+}
+ 
+}
+}
