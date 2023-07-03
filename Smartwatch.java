b76cc896872bbba9cc86c53251c0b5ec5ae92828
unknown <truptic.xworkz@gmail.com>
2023-07-04 00:04:04 +0530

    test push

---
+++ Smartwatch.java
@@ -0,0 +1,66 @@
+public class Smartwatch{
+String name;
+String brand;
+String color;
+int price;
+int size;
+String shape;
+String quality;
+String quatity;
+String typeOfTiming;
+String waterProof;
+String voiceCommand;
+String display;
+String sleepTracking;
+String brandSeries;
+int weight;
+String bandColor;
+String typeOfBand;
+int lengthOfWatch;
+float processor;
+float sound;
+public static void main(String[] args){
+
+Smartwatch obj = new Smartwatch("mi",2000,"pink");
+
+}
+
+public Smartwatch(String brand,int price,String color)
+{
+this.brand=brand;
+this.price=price;
+this.color=color;
+System.out.println(brand);
+System.out.println(price);
+System.out.println(color);
+}
+public static void makeAndRecievingCall()
+{
+}
+public static void playMusic()
+{
+}
+public static void accessNotification()
+{
+}
+public static void takeingPhoto()
+{
+}
+public static void weather()
+{
+}
+public static void alaram()
+{
+}
+public static void heartBeat()
+{
+}
+public static void wifiConncetion(){
+}
+public static void breathing(){
+
+}
+public static void smartNavigation()
+{
+}
+}
